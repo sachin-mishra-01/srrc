d9a826a4b4802f0520f2709683f9667e2861f43d
sachin mishra <msachinmishra75@gmail.com>
2025-08-22 18:15:54 +0530

    Delete SkillMintt directory

--- SkillMintt/svr/utils/gentkn.js
+++
@@ -1,8 +0,0 @@
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export const gentkn = (uid , usnm)=>{
-    return jwt.sign({id:uid , username: usnm },process.env.JWT_SECRET,{expiresIn:process.env.JWT_EXPIRES_IN});
-};
