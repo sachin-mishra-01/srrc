eedfb28fd68f18b56527b9ed1ff85023e011f2e5
sachin mishra <msachinmishra75@gmail.com>
2026-01-30 13:39:29 +0530

    Update server.js

--- SkillMintt/svr/server.js
+++ SkillMintt/svr/server.js
@@ -19,7 +19,9 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(cookieParser());
-
+app.get("/", (req, res) => {
+  res.status(200).send("OK");
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/find" , findRoutes);
 
